e856746f4a73f8d49f3bd48f10de63ad5c0de9d8
--add <chintak.soni75@gmail.com>
2017-09-05 11:54:33 +0530

    django rest tutorial project commit

---
+++ snippets/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+    class Meta:
+        model = Snippet
+        fields = ('url', 'id', 'title', 'code', 'highlight', 'linenos', 'language', 'style','owner')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all() )
+
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'snippets')
